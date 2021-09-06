70627051639ae8db276058f94dcef1b721512b7d
masdika ID <58813839+masdikaid@users.noreply.github.com>
2021-09-06 22:45:13 +0700

    update: change env directory

--- config/config.go
+++ config/config.go
@@ -8,8 +8,8 @@ import (
 
 func init() {
 	// load config
-	err := godotenv.Load("../.env")
+	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatal("Error loading .env file", err)
+		log.Fatal("Error loading .env file : ", err)
 	}
 }
