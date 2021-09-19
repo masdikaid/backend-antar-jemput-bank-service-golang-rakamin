b3eff99d60d5d372b8584bd9337bb197f84542a8
masdika ID <masdikaid@gmail.com>
2021-09-19 19:31:53 +0700

    update: new logout feature (need refactoring)

--- internal/api/auth.go
+++ internal/api/auth.go
@@ -5,8 +5,10 @@ import (
 	"backend-a-antar-jemput/internal/repository"
 	"backend-a-antar-jemput/internal/service"
 	"backend-a-antar-jemput/tools/helper"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
 )
 
 var AuthService = service.AuthService{AuthRepository: repository.AuthRepositoryMysql{}, SessionRepository: repository.SessionRepositoryMysql{}}
@@ -27,12 +29,28 @@ func Login(c *fiber.Ctx) error {
 }
 
 func Logout(c *fiber.Ctx) error {
-	response := struct {
-		Status  int    `json:"status"`
-		Message string `json:"message"`
-	}{
-		Status:  fiber.StatusOK,
-		Message: "Berhasil logout",
+	contr := contract.LoginResponse{}
+
+	tokenya := c.Get(fiber.HeaderAuthorization)[len("Bearer "):]
+
+	token, err := jwt.Parse(tokenya, JwtFunc)
+	if err != nil {
+		return helper.JsonResponseFailBuilder(c, fiber.StatusBadRequest, err.Error())
 	}
-	return c.JSON(response)
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		contr.SID = claims["sid"].(string)
+	} else {
+		return helper.JsonResponseFailBuilder(c, fiber.StatusBadRequest, err.Error())
+	}
+
+	err = AuthService.Logout(&contr)
+	if err != nil {
+		return helper.JsonResponseFailBuilder(c, fiber.StatusBadRequest, err.Error())
+	}
+	return helper.JsonResponseOkBuilder(c, fiber.StatusOK, "Success Logout", nil)
+}
+
+func JwtFunc(claim *jwt.Token) (interface{}, error) {
+	return []byte(os.Getenv("SECRET")), nil
 }
