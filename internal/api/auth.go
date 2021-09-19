98183135ca1457a5c0442c2e6683b247bff0485c
masdika ID <masdikaid@gmail.com>
2021-09-19 17:58:13 +0700

    update: login handler build

--- internal/api/auth.go
+++ internal/api/auth.go
@@ -1,19 +1,29 @@
 package api
 
 import (
+	"backend-a-antar-jemput/internal/contract"
+	"backend-a-antar-jemput/internal/repository"
+	"backend-a-antar-jemput/internal/service"
+	"backend-a-antar-jemput/tools/helper"
+
 	"github.com/gofiber/fiber/v2"
 )
 
+var AuthService = service.AuthService{AuthRepository: repository.AuthRepositoryMysql{}, SessionRepository: repository.SessionRepositoryMysql{}}
+
 // example route for login
 func Login(c *fiber.Ctx) error {
-	response := struct {
-		Status  int    `json:"status"`
-		Message string `json:"message"`
-	}{
-		Status:  fiber.StatusOK,
-		Message: "berhasil login",
+	var contr contract.LoginRequest
+	err := c.BodyParser(&contr)
+	if err != nil {
+		return helper.JsonResponseFailBuilder(c, fiber.StatusBadRequest, "Bad Request")
 	}
-	return c.JSON(response)
+	var token string
+	token, err = AuthService.Login(contr)
+	if err != nil {
+		return helper.JsonResponseFailBuilder(c, fiber.StatusBadRequest, err.Error())
+	}
+	return helper.JsonResponseOkBuilder(c, fiber.StatusAccepted, "success", token)
 }
 
 func Logout(c *fiber.Ctx) error {
