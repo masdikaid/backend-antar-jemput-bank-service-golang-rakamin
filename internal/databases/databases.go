d42068b1fa499b637ab09951158b8b3897de3d7b
masdika ID <masdikaid@gmail.com>
2021-09-19 17:55:52 +0700

    update: make databases load automaticaly

--- internal/databases/databases.go
+++ internal/databases/databases.go
@@ -11,6 +11,10 @@ var (
 	DBCon *gorm.DB
 )
 
+func init() {
+	Load()
+}
+
 func Load() {
 	var err error
 	DBCon, err = gorm.Open(mysql.Open(os.Getenv("DB_USERNAME") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DB_DATABASE") + "?charset=utf8mb4&parseTime=True&loc=Local"))
