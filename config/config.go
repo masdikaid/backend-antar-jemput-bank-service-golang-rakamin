cc463a45daf64bd9ebb26c2b8cb4d10aabd8bacd
masdika ID <masdikaid@gmail.com>
2021-10-03 18:21:40 +0700

    add dotenv

--- config/config.go
+++ config/config.go
@@ -1,9 +1,15 @@
 package config
 
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
 func init() {
 	// load config
-	// err := godotenv.Load(".env")
-	// if err != nil {
-	// 	log.Fatal("Error loading .env file : ", err)
-	// }
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file : ", err)
+	}
 }
