bbd8b7b7a8385721ceda337d42b91d691438af12
masdika ID <masdikaid@gmail.com>
2021-10-03 17:51:08 +0700

    removing port

--- internal/databases/databases.go
+++ internal/databases/databases.go
@@ -17,7 +17,7 @@ func init() {
 
 func Load() {
 	var err error
-	DBCon, err = gorm.Open(mysql.Open(os.Getenv("DB_USERNAME") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DB_DATABASE") + "?charset=utf8mb4&parseTime=True&loc=Local"))
+	DBCon, err = gorm.Open(mysql.Open(os.Getenv("DB_USERNAME") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("DB_HOST") + ")/" + os.Getenv("DB_DATABASE") + "?charset=utf8mb4&parseTime=True&loc=Local"))
 	if err != nil {
 		panic("failed to connect database")
 	}
